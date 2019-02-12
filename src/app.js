5ef7dfdc3d968f540576e9a8c8999dd63310f588
Jakub Kania <kubak.15@gmail.com>
2019-02-12 18:56:14 +0100

    change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import Hello from "./hello";
-//import "./style.scss";
+import "./styles.scss";
 import MyProvider from "./MyProvider";
 
 class App extends Component {
